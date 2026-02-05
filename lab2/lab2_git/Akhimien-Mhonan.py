39d08475ce64e64fab12a057ac132af8596d83f4
Efuah Akhimien-Mhonan <efuah.akhimien-mhonan29@stu.bmcc.cuny.edu>
2026-02-04 21:53:38 -0500

    Add Lab 1 and Lab 2 folders properly

---
+++ lab2/lab2_git/Akhimien-Mhonan.py
@@ -0,0 +1,10 @@
+"""
+Efuah Akhimien-Mhonan
+Lab 2: Git and GitHub repository
+"""
+username = input("Enter a username: ")
+print("Welcome to Git commands ", username)
+
+num = 8
+print(f"Double of number {num} is {num*2}")
+
